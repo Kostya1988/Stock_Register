b7ea57598cc8246d9a395e336dff7dd115c8b73c
Kostya1988 <46385286+Kostya1988@users.noreply.github.com>
2019-10-30 00:50:14 +0300

    Update Stocks.java

--- src/main/java/Test/Stock_Register/Model/Stocks.java
+++ src/main/java/Test/Stock_Register/Model/Stocks.java
@@ -10,23 +10,23 @@ import java.sql.Date;
 @Table(name = "Registr")
 public class Stocks {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(name = "comment")
     private String comment;
     @Column(name = "amount_Authorized_Capital")
-    private int amount_Authorized_Capital;
+    private int amountAuthorizedCapital;
     @Column(name = "eDRPOU_Institutions")
-    private int eDRPOU_Institutions;
+    private int eDRPOUInstitutions;
     @Column(name = "quantity")
     private int quantity;
     @Column(name = "nominal_value")
-    private double nominal_value;
+    private double nominalValue;
     @Column(name = "total_Face_Value")
-    private double total_Face_Value = nominal_value * Double.valueOf(quantity);
+    private double totalFaceValue = nominalValue * Double.valueOf(quantity);
     @Column(name = "state_duty_paid")
-    private double state_duty_paid;
+    private double stateDutyPaid;
     @Column(name = "release_Date")
-    private Date release_Date;
+    private Date releaseDate;
 
 }
